501decc3aa0c42e71ed47127be13d263acaf91b9
Carlos Gonzalez <carlosegl10@gmail.com>
2021-04-01 16:49:05 -0400

    Delete de folders listo

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import display_folders
-
+from .views import display_folders, delete_folder
 
 
 urlpatterns = [
     path('folders/', display_folders , name='folders'),
+    path('folders/delete', delete_folder, name='delete_folder')
 ]
--- to_do_list/views.py
+++ to_do_list/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Folder
 # Create your views here.
 @login_required
 def display_folders(request):
-    print(request.user.id)
+  #  Folder.objects.all().delete()
+    if request.POST:
+        if request.POST.get('new_folder') != "":
+            new = Folder(name=request.POST.get('new_folder'), user_id = request.user)
+            new.save()
+    folders = Folder.objects.filter(user_id = request.user)
+    print(folders)
+    return render(request, 'folders_atoms/base.html', {"folders": folders})
+
+@login_required
+def delete_folder(request):
+    if request.POST.get('delete_id'):
+        Folder.objects.filter(folder_id = request.POST['delete_id']).delete()
+
+    return HttpResponse('')
+
+
+
