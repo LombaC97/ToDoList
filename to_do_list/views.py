4cf70bbb360c775ce765bb2fe9f194de40de0621
Carlos Gonzalez <carlosegl10@gmail.com>
2021-04-02 08:23:09 -0400

    Header template para incluir

--- to_do_list/views.py
+++ to_do_list/views.py
@@ -11,15 +11,13 @@ def display_folders(request):
             new = Folder(name=request.POST.get('new_folder'), user_id = request.user)
             new.save()
     folders = Folder.objects.filter(user_id = request.user)
-    print('FOOOOOLDEEEEEEEEEERSSSSSSSSSS')
-    print(folders)
+  
     return render(request, 'folders_atoms/base.html', {"folders": folders, "username": request.user.username})
 
 @login_required
 def delete_folder(request):
     if request.POST.get('delete_id'):
-        print("AAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-        print(request.POST['delete_id'])
+       
         Folder.objects.filter(folder_id = request.POST['delete_id']).delete()
 
     return HttpResponse('')
