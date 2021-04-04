91027a37fd51f9b3ae2c73b5423895ce62d83395
LombaC97 <38764931+LombaC97@users.noreply.github.com>
2021-04-03 21:29:11 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ sudo apt install python3-pip
 sudo apt install python3-venv
 sudo apt install python3-dev libpq-dev
 python3 -m venv entorno 
+python -m venv entorno
 source entorno/bin/activate
 pip install -r requirements.txt
 entorno/bin/python manage.py runserver
